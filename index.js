b6f53d25c14336dc07f7c534818842358191adaa
waleedfarraj <waleed.farraj95@gmail.com>
2020-09-06 18:16:33 +0300

    class01 completed

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+const Input = require('./lib/input');
+const Notes = require('./lib/notes');
+
+const arg = new Input();
+// eslint-disable-next-line no-unused-vars
+const note = new Notes(arg);
