784ba58bf688ce1fb2004b7e220aa86ef50623c6
waleedfarraj <waleed.farraj95@gmail.com>
2020-09-08 23:01:56 +0300

    stuck so far only schema and env and some changes to input

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 'use strict';
+require('dotenv').config()
 const Input = require('./lib/input');
 const Notes = require('./lib/notes');
 
 const arg = new Input();
+const mongoose = require('mongoose');
+const Food = require('./lib/model/notes.schema');
+
+// this should be in your .env file 
+const mon = (process.env.MONGOOSE_URL);
+
+mongoose.connect(mon, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
 
 const note = new Notes(arg);
--- tests/input.test.js
+++ tests/input.test.js
@@ -25,6 +25,34 @@ describe('Input Module', ()=> {
        let args = { _: [], a: 'HELLO Darkness my old friend ' }
         expect(testInput.valid(args)).toBeTruthy();
     });
+    it('add() with correct method and a msg', () =>{
+        let options = new Notes();
+        let input ={method:{action: 'add', payload: 'hiii', category: 'school'}};
+        options.add(input);
+        expect(console.log).toHaveBeenCalled();
+      });
+    
+      it('delete()', () =>{
+        let options = new Notes();
+        let input ={method:{action: 'delete', id: '5ec42429d7c4e1070fc613c6'}};
+        options.delete(input);
+        expect(console.log).toHaveBeenCalled();
+      });
+    
+      it('list()', () =>{
+        let options = new Notes();
+        let input ={method:{action: 'list', category: 'school'}};
+        options.list(input);
+        expect(console.log).toHaveBeenCalled();
+      });
+    
+      it('displayStyle()', () =>{
+        let options = new Notes();
+        let input =[{text:'hii',category:'school',_id:'5ec42429d7c4e1070fc613c6'}];
+        options.displayStyle(input);
+        expect(console.log).toHaveBeenCalled();
+      });
+    
     
   
 
